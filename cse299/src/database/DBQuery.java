4ba34f1a076b17a25fdc2e59622b371359500be0
Sourav-Debnath <souravdebnath.ic@gmail.com>
2020-02-14 01:07:49 +0600

    Reset Password Query Added

--- cse299/src/database/DBQuery.java
+++ cse299/src/database/DBQuery.java
@@ -3,17 +3,52 @@ package tenantPortal;
 import java.sql.*;
 
 public class DBQuery {
-	DBConnectionSingleton objDBConnection=DBConnectionSingleton.getInstance();
-	Connection con=objDBConnection.getConnection();
+	Connection con;
 	
-	public void resetPassword(){		
-		
+	//constructor
+	public DBQuery() {
+		DBConnectionSingleton objDBConnection=DBConnectionSingleton.getInstance();
+		con=objDBConnection.getConnection();
+	}
+	
+	
+	public boolean isEmailExists(String email){
+		String query="Select id from user where email='"+email+"'";
+		boolean flag=false;
 		try {
-		    String query="";
-		    PreparedStatement ps=con.prepareStatement(query);
+			PreparedStatement ps=con.prepareStatement(query);
+			ResultSet rs=ps.executeQuery();
+			int size=0;
+			if(rs != null) {
+				rs.last();
+				size = rs.getRow();
+			}
+			flag=(size == 1);			
 		} catch (Exception e) {
 		    System.out.println(e);
 		}
+		return flag;
+	}
+	
+	public void resetPassword(String email) {
+		if(isEmailExists(email)) {
+			String query="Select pass from user where email='"+email+"'";
+			try {
+				PreparedStatement ps=con.prepareStatement(query);
+				ResultSet rs=ps.executeQuery();
+				rs.next();
+				
+				//-------------------Fix here to get the password via email
+			    System.out.println("Password :"+rs.getString("pass"));			    
+			}catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		DBQuery objDBQuery=new DBQuery();
+		objDBQuery.resetPassword("mokles@hotmail.com");
 	}
 
 }
